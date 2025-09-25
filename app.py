44b5f52d41e263fd5b4fa6bfe04ea8a564269fcb
Krishna Monani <krishnamonani004@gmail.com>
2025-09-25 17:21:41 +0530

    update title for better understanding

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ def hello():
     return f"""
     <html>
         <head>
-            <title>DEVELOPMENT</title>
+            <title>DEVELOPMENT infra</title>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <style>{GLOBAL_CSS}</style>
         </head>
